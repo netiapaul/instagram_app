8c6fefd8af7b7957be3ada64d8d2ac424492f9e8
netiapaul <netiapaul23@gmail.com>
2021-03-31 23:54:42 +0300

    added correct replica of registration page

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,8 +1,9 @@
 import React from "react";
 // import Illustration from "../../assets/images/registration2.jpg";
 // import "../../assets/libs/bootstrap-5.0.0/css/bootstrap.min.css";
-import Logo from "../../assets/images/registration.png";
 import "./register.css";
+import AppStore from "../../assets/images/apple.png";
+import PlayStore from "../../assets/images/google.png";
 import { useFormik } from "formik";
 
 const validate = (values) => {
@@ -12,6 +13,16 @@ const validate = (values) => {
     errors.password = "Required";
   } else if (values.password.length > 15) {
     errors.password = "Must be 15 characters or less";
+  }
+  if (!values.fullname) {
+    errors.fullname = "Required";
+  } else if (values.fullname.length > 20) {
+    errors.fullname = "Must be 20 characters or less";
+  }
+  if (!values.username) {
+    errors.username = "Required";
+  } else if (values.username.length > 20) {
+    errors.username = "Must be 20 characters or less";
   }
 
   if (!values.email) {
@@ -27,6 +38,8 @@ const Register = () => {
   const formik = useFormik({
     initialValues: {
       email: "",
+      fullname: "",
+      username: "",
       password: "",
     },
     validate,
@@ -35,13 +48,32 @@ const Register = () => {
     },
   });
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-sm">
-          <div class="card">
-            <div class="card-body">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          <div className="card form-card mt-5">
+            <div className="card-body">
+              <div className="text-center mb-3 fw-bolder text-muted">
+                <h2>
+                  Signup to see photos and videos
+                  <br /> from your friends
+                </h2>
+              </div>
+              <div className="text-center mb-3">
+                <button
+                  type="button"
+                  className="btn btn-primary text-white fs-6 fb-login"
+                >
+                  Login in with Facebook
+                </button>
+              </div>
+              <div className="d-flex justify-content-evenly text-muted">
+                <hr align="left" width="40%" />
+                <p>OR</p>
+                <hr align="right" width="40%" />
+              </div>
               <form onSubmit={formik.handleSubmit}>
-                <div class="mb-3">
+                <div className="mb-3">
                   <input
                     id="email"
                     className="form-control rounded-0"
@@ -50,14 +82,46 @@ const Register = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.email}
-                    placeholder="email"
+                    placeholder="Email"
                   />
 
                   {formik.touched.email && formik.errors.email ? (
                     <div className="text-danger">{formik.errors.email}</div>
                   ) : null}
                 </div>
-                <div class="mb-3">
+                <div className="mb-3">
+                  <input
+                    id="fullname"
+                    className="form-control rounded-0"
+                    name="fullname"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.fullname}
+                    placeholder="Full Name"
+                  />
+
+                  {formik.touched.fullname && formik.errors.fullname ? (
+                    <div className="text-danger">{formik.errors.fullname}</div>
+                  ) : null}
+                </div>
+                <div className="mb-3">
+                  <input
+                    id="username"
+                    className="form-control rounded-0"
+                    name="username"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.username}
+                    placeholder="User Name"
+                  />
+
+                  {formik.touched.username && formik.errors.username ? (
+                    <div className="text-danger">{formik.errors.username}</div>
+                  ) : null}
+                </div>
+                <div className="mb-3">
                   <input
                     id="password"
                     className="form-control rounded-0"
@@ -73,31 +137,103 @@ const Register = () => {
                     <div className="text-danger">{formik.errors.password}</div>
                   ) : null}
                 </div>
-                <div>
-                  <div className="d-grid mb-3">
-                    <button type="submit" className="btn btn-primary">
-                      Log In
-                    </button>
-                  </div>
+                <div className="d-grid mb-3">
+                  <button type="submit" className="btn btn-primary">
+                    Sign Up
+                  </button>
                 </div>
-
-                <div className="d-flex justify-content-evenly">
-                  <hr align="left" width="40%" />
-                  <p>OR</p>
-                  <hr align="right" width="40%" />
-                </div>
-                <div className="text-center">
-                  <a href="">Login in with Facebook</a>
+                <div className="text-center mb-3 text-muted policy">
+                  <p>
+                    By signing up, you agree to our Terms , Data
+                    <br /> Policy and Cookies Policy .
+                  </p>
                 </div>
               </form>
             </div>
           </div>
-          <div class="card my-2 text-center">
-            <div class="card-body">
-              Don't have an account? <a href="">Sign Up</a>
+          <div className="card my-2 text-center">
+            <div className="card-body">
+              <p className="my-3 sign-up">
+                Don't have an account? <a href="">Sign Up</a>
+              </p>
             </div>
           </div>
-          <p className="text-center">Get the App</p>
+          <p className="text-center my-4">Get the App.</p>
+          <div className="container">
+            <div className="row row-cols-2 d-flex justify-content-center">
+              <div className="col-sm-2 mb-2 d-flex justify-content-center">
+                <img
+                  src={AppStore}
+                  className="img-fluid px-0"
+                  alt="app Store"
+                />
+              </div>
+              <div className="col-sm-2 mb-2 d-flex justify-content-center">
+                <img
+                  src={PlayStore}
+                  className="img-fluid px-0"
+                  alt="play Store"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container px-1">
+            <div className="row row-cols-5 row-cols-md-10 d-flex justify-content-center p-2 about">
+              <div className="col-sm-1">
+                <p>About</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Blog</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Jobs</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Help</p>
+              </div>
+              <div className="col-sm-1">
+                <p>API</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Privacy</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Terms</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Top Accounts</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Hashtags</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Locations</p>
+              </div>
+            </div>
+            <div className="row row-cols-4 row-cols-md-10 d-flex justify-content-center p-2 about">
+              <div className="col-sm-1">
+                <p>Beauty</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Dance & Performance</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Fitness</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Food & Drink</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Home & Garden</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Music</p>
+              </div>
+              <div className="col-sm-1">
+                <p>Visual Arts</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
